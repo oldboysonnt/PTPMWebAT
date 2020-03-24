1c630fef70561be4aa0105b45f897782fd41dc9d
Thanh Son <ngthanhson0701@gmail.com>
2020-03-24 16:50:23 +0700

    hoan thanh

--- copy.js
+++ copy.js
@@ -1,9 +1,30 @@
 let codeTagsHC = document.getElementsByTagName("code");
 let codeTags = [].slice.call(codeTagsHC);
 
+
+function selectText(node) {
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(node);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else {
+        console.warn("Could not select text in node: Unsupported browser.");
+    }
+}
+
+// const clickable = document.querySelector('.click-me');
+// clickable.addEventListener('click', () => selectText('target'));
+
 for (let tag of codeTags) {
     tag.addEventListener("dblclick", function () {
-
+        console.log(tag);
+        selectText(tag);
         navigator.clipboard.writeText(tag.innerText).then(function () {
             console.log("clipboard successfully set");
             navigator.clipboard.readText().then(function (clipText) {
